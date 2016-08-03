2e185022bb55b4b3cde7779f5bcc531c5160ef8d
ian <chu2815@hotmail.com>
2016-08-03 11:52:16 +0800

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -12,4 +12,30 @@ $(document).ready(function() {
 		$('.user-right').toggle('slow');
 	});
 
+        $("#language").click(function() {
+        $('.language-list').toggle('slow');
+    });
+        $("#dollar").click(function() {
+        $('.dollar-list').toggle('slow');
+    });
+        
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop: true
+    });
+        var swiper = new Swiper('.swiper-container2', {
+        pagination: '.swiper-pagination2',
+        nextButton: '.next',
+        prevButton: '.prev',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop: true
+    });
+
 });
