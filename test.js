4465b15150f6bba1321c03efca1e429796894adb
ian <chu2815@hotmail.com>
2017-05-16 20:41:26 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
-       var Greeting = React.createClass({
+      var Greeting = React.createClass({
         render: function() {
           return (
-            <h1>朱朱爬蟲測試爬react 2017/5/15 測試chuian測試爬蟲測試爬react測試chuian測試爬蟲測試爬react測試chuian測試</h1>
+            <h1>爬蟲測試爬react測試chuian測試爬蟲測試爬react測試chuian測試爬蟲測試爬react測試chuian測試</h1>
           )
         }
       });
-
-export default Greeting;
+      ReactDOM.render(
+        <Greeting/>,
+        document.getElementById('greeting-div')
+      );
